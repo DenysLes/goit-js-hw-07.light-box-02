aedd40d64f3ffc37af3bedc98bee39632d35dfff
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-01-19 21:51:29 +0200

    Update gallery .html and gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const containerItemsGalleryEl = document.querySelector(".gallery");
+console.log(containerItemsGalleryEl);
+
+const markupCardsOfGalleryEl = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+});
+containerItemsGalleryEl.insertAdjacentHTML('beforeend', markupCardsOfGalleryEl.join(''));
